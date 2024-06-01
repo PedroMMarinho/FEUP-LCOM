22f1a862396904a01dc84694a1cd0b2f126cdb2e
Pedro Marinho <pmiguel.marinho@gmail.com>
2024-06-01 11:35:23 +0100

    Some documentation

--- src/labs/graphicsMacros.h
+++ src/labs/graphicsMacros.h
@@ -1,10 +1,17 @@
 #pragma once
 #include <lcom/lcf.h>
 
-
+/**
+ * @brief BIOS interrupt number for video services.
+ *
+ * This macro defines the interrupt number for invoking BIOS video services.
+ */
 #define INT 0x10;
-
-
+/**
+ * @brief Function to set or get the display start address in VBE.
+ *
+ * This macro defines the function number for setting or getting the display start address in the VESA BIOS Extensions (VBE).
+ */
 #define SET_GET_DISPLAY_START 0x4F07;
 
 
--- src/labs/mouse.h
+++ src/labs/mouse.h
@@ -4,25 +4,73 @@
 
 #include <stdint.h>
 
-
+/**
+ * @brief Subscribes and enables Mouse interrupts.
+ * 
+ * @param bit_no Address of memory to be initialized with the bit number to be set in the mask returned upon an interrupt.
+ * 
+ * Sets var error to 1 if there was an error
+*/
 void (mouse_ih)();
 
+/**
+ * @brief Checks if there was an error in the interrupt handler.
+ * 
+ * @return true if there was an error, false otherwise.
+*/
 bool mouseError();
 
+/**
+ * @brief Subscribes and enables Mouse interrupts.
+ * 
+ * @param bit_no Address of memory to be initialized with the bit number to be set in the mask returned upon an interrupt.
+ * 
+ * @return 0 upon success, one otherwise.
+*/
 int(mouse_subscribe)(uint8_t *bit_no);
 
+/**
+ * @brief Unsubscribes Mouse interrupts.
+ * 
+ * @return 0 upon success, one otherwise.
+*/
 int(mouse_unsubscribe)(); 
 
-uint8_t get_mouse_byte();
-
+/**
+ * @brief Checks if the packet is complete.
+ * 
+ * @return true if the packet is complete, false otherwise.
+*/
 int complete_packet();
 
+/**
+ * @brief Checks if the packet is complete.
+ * 
+ * @return true if the packet is complete, false otherwise.
+*/
 bool isPacketComplete();
 
+/**
+ * @brief Gets the packet read from the output buffer.
+ * 
+ * @return The packet read from the output buffer.
+*/
 struct packet getMousePacket();
 
+/**
+ * @brief Updates the packet with the new byte.
+ * 
+ * @param mouseByte The new byte to be added to the packet.
+ * 
+ * @return 0 upon success, one otherwise.
+*/
 int updatePacket(uint8_t mouseByte);
 
+/**
+ * @brief Disables data reporting.
+ * 
+ * @return 0 upon success, one otherwise.
+*/
 int disableDataReporting();
 
 #endif
